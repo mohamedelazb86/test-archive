53d81ef4a7802fd018e1728770e617588cf7f806
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-11-06 19:32:52 +0200

    add static signup

--- project/urls.py
+++ project/urls.py
@@ -21,6 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('accounts/',include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    
     path('admin/', admin.site.urls),
 ]
 
